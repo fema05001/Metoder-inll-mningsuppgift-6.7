7363ae967d13d574daea8b755b7d408319c6df16
Elev <Elev@TEB125102>
2023-03-22 08:54:48 +0100

    Add project files.

---
+++ Metoder inllämningsuppgift 6.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace metoder
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Primtal();
+        }
+        static void Primtal()
+        {
+            Console.WriteLine("Skriv ett valfrit heltal");
+            string a =Console.ReadLine();
+            int b = int.Parse(a);
+            Console.WriteLine(" ");
+            if (b % 3 == 0 || b % 2 == 0)
+            {
+                Console.WriteLine("Talet du skrev in är inte ett primtal");
+            }
+
+           else if (b % 3 != 0 || b == 3 & b % 2 != 0 || b == 2)
+            {
+                Console.WriteLine("Talet du skrev in är ett primtal");
+            }
+           
+        }
+    }
+}
