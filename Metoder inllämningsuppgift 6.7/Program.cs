3d1e4dff671bcb26361d07f1a15c66be2b7f256a
fema05001 <112701640+fema05001@users.noreply.github.com>
2023-03-22 09:17:25 +0100

    Update Program.cs

--- Metoder inllämningsuppgift 6.7/Program.cs
+++ Metoder inllämningsuppgift 6.7/Program.cs
@@ -13,7 +13,7 @@ namespace metoder
             string a =Console.ReadLine();
             int b = int.Parse(a);
             Console.WriteLine(" ");
-            if (b % 3 == 0 & b!=3 || b % 2 == 0 & b!=3 || b % 5 ==0)
+            if (b % 3 == 0 & b!=3 || b % 2 == 0 & b!=2 || b % 5 ==0)
             {
                 Console.WriteLine("Talet du skrev in är inte ett primtal");
             }
